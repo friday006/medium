c62e56a5d733ed222c38baf55ee0c9e790e74e1b
Priyankar Nigam <priyankarnigam786@gmail.com>
2025-03-10 14:47:06 +0530

    temp live

--- frontend/src/hooks/index.tsx
+++ frontend/src/hooks/index.tsx
@@ -2,7 +2,7 @@ import {useState, useEffect} from 'react'
 import axios from 'axios'
 import {BACKEND_URL} from '../config'
 
-interface Blog {
+export interface Blog {
             "content": string;
             "title": string;
             "id": string;
--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,16 +1,17 @@
 import {useBlog} from '../hooks'
 import {useParams} from 'react-router-dom'
+import {FullBlog} from '../components/FullBlog'
 
 export const Blog = ()=>{
     const {id} = useParams()
     const {loading, blog} = useBlog({id: id||""});
 
-        if(loading){
+        if(loading || !blog){
             return <div>
                 loading..
             </div>
         }
     return(<>
-    blog details
+    <FullBlog blog={blog}/>
     </>)
 }
