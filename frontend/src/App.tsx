16b7ef4b68685929f2263e3bf8e6cce1cee8afd6
Priyankar Nigam <priyankarnigam786@gmail.com>
2025-03-17 18:43:23 +0530

    added protected routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,73 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom"
-import {Signup} from "./pages/Signup"
-import {Signin} from "./pages/Signin"
-import {Blog} from "./pages/Blog"
-import {Blogs} from "./pages/Blogs"
-import {Publish} from "./pages/Publish"
+import { ReactNode } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { Blog } from "./pages/Blog";
+import { Blogs } from "./pages/Blogs";
+import { Publish } from "./pages/Publish";
 
 function App() {
-
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Blogs/>}/>
-          <Route path="/signup" element={<Signup/>}/>
-          <Route path="/signin" element={<Signin/>}/>
-          <Route path="/blog/:id" element={<Blog/>}/>
-          <Route path="/blogs" element={<Blogs/>}/>
-          <Route path="/publish" element={<Publish/>}/>
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        {/* Protected routes */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Blogs />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/blog/:id"
+          element={
+            <ProtectedRoute>
+              <Blog />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/blogs"
+          element={
+            <ProtectedRoute>
+              <Blogs />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/publish"
+          element={
+            <ProtectedRoute>
+              <Publish />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Public routes */}
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
+
+interface ProtectedRouteProps {
+  children: ReactNode;
+}
+
+const ProtectedRoute = ({ children }:ProtectedRouteProps) => {
+  // Fetch the token from localStorage - adjust the key as necessary.
+  const token = localStorage.getItem("token");
+
+  // If there's no token, send the user to the signin page.
+  if (!token) {
+    return <Navigate to="/signin" replace />;
+  }
+
+  // Otherwise, allow them to see the protected content.
+  return children;
+};
